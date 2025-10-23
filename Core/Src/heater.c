b28a0791ae354ba41279c7e94b545fb75e19dce1
zwb800 <zwb800@gmail.com>
2025-10-23 14:21:22 +0800

    pwm frequency to 70khz

--- Core/Src/heater.c
+++ Core/Src/heater.c
@@ -8,6 +8,6 @@
 
     void SetWaterHearterPower(uint8_t pwm)
     {
-        uint32_t p = 2000 * pwm / 255;
+        uint32_t p = 300 * pwm / 255;
         __HAL_TIM_SetCompare(&htim4, TIM_CHANNEL_4, p);
     }
