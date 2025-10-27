476cc19d2582abf8d585fceaea3b0038dff9a95d
zwb800 <zwb800@gmail.com>
2025-10-27 23:57:43 +0800

    pwm frequency to 100hz

--- Core/Src/heater.c
+++ Core/Src/heater.c
@@ -8,6 +8,6 @@
 
     void SetWaterHearterPower(uint8_t pwm)
     {
-        uint32_t p = 300 * pwm / 255;
+        uint16_t p = 1000 * pwm / 255;
         __HAL_TIM_SetCompare(&htim4, TIM_CHANNEL_4, p);
     }
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -42,9 +42,9 @@ void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 2-1;
+  htim4.Init.Prescaler = 840-1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 300-1;
+  htim4.Init.Period = 1000-1;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
