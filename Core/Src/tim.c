917f9ee9993f59ed4da6755bd4ace2714fa9f91b
zwb800 <zwb800@gmail.com>
2025-11-12 10:13:19 +0800

    enable autoreload preload freq to 200hz

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -42,11 +42,11 @@ void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 840-1;
+  htim4.Init.Prescaler = 420-1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 1000-1;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
   {
     Error_Handler();
